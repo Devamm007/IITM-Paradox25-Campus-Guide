bc9168ef65783eef3d92d1f5fa175ccbfd5e2081
Devamm007 <shahdevam49@gmail.com>
2025-06-04 07:50:01 +0000

    Final 4.5

--- backend/index.js
+++ backend/index.js
@@ -3,17 +3,19 @@ dotenv.config();
 import cors from "cors";
 
 import express from "express";
-import mongoose from "mongoose";
+// import mongoose from "mongoose";
 
 import Event from "./models/event.js";
 import Miscellaneous from "./models/miscellaneous.js";
+import { promises as fs } from 'fs';
+
 // import nunjucks from "nunjucks";
 // import path from 'path';
 
 const app = express();
 
 app.use(cors());
-const MONGO_URI = process.env.MONGODB_URI;
+// const MONGO_URI = process.env.MONGODB_URI;
 const PORT = process.env.PORT;
 const secret = process.env.secret;
 
@@ -45,22 +47,36 @@ const env = nunjucks.configure(path.join(__dirname, 'views'), {
 app.use(express.urlencoded({ extended: false }));
 app.use("/static", express.static("static"));
 
-// mongo connect
-mongoose
-  .connect(MONGO_URI)
-  .then(() => {
-    console.log("MongoDB connected successfully");
-  })
-  .catch((e) => {
-    console.log("Error connecting MongoDB" + e);
-  });
+// // mongo connect
+// mongoose
+//   .connect(MONGO_URI)
+//   .then(() => {
+//     console.log("MongoDB connected successfully");
+//   })
+//   .catch((e) => {
+//     console.log("Error connecting MongoDB" + e);
+//   });
+
+
+// Helper function to load JSON data
+async function loadJSONData(filename) {
+  try {
+    const dataDir = path.join(__dirname, 'data');
+    const filePath = path.join(dataDir, filename);
+    const data = await fs.readFile(filePath, 'utf-8');
+    return JSON.parse(data);
+  } catch (error) {
+    console.error(`Error loading ${filename}:`, error);
+    return [];
+  }
+}
 
 app.get("/", (req, res) => {
   return res.render("home");
 });
 
 app.get("/events", async (req, res) => {
-  const events = await Event.find({});
+  const events = await loadJSONData('events.json');
   const query = "";
   events.sort((a, b) => new Date(a.startTime) - new Date(b.startTime));
   // console.log(events);
@@ -78,114 +94,28 @@ app.get("/webteam", (req, res) => {
   return res.render("webteam");
 });
 
-// secret route
-app.get("/shakalakaboomboom", async (req, res) => {
-  const events = await Event.find({});
-  events.sort((a, b) => new Date(a.startTime) - new Date(b.startTime));
-  res.render("admin", { events });
-});
-
-// post route for creation
-app.post("/shakalakaboomboom", async (req, res) => {
-  const name = req.body.name;
-  const startTime = req.body.start;
-  const endTime = req.body.end;
-  const locName = req.body.locName;
-  const lat = Number(req.body.lat);
-  const long = Number(req.body.long);
-
-  try {
-    // secret for us
-    if (req.body.secret !== secret) {
-      return res.status(403).json({ msg: "Unauthorized" });
-    }
-
-    await Event.create({
-      name,
-      startTime,
-      endTime,
-      locations: [{ name: locName, latitude: lat, longitude: long }],
-      documentLink: "example",
-      organiserEmail: "example@ds.study.iitm.ac.in",
-    });
-    alert("created event successfully");
-    return res.redirect("/shakalakaboomboom");
-  } catch (e) {
-    res.status(500).json({ error: e });
-  }
-});
-
-// get route for edit
-app.get("/shakalakaboomboom/edit/:id", async (req, res) => {
-  const id = req.params.id;
-  const event = await Event.findById(id);
-
-  if (!event) {
-    return res.status(404).json({ msg: "event not found!" });
-  }
-  res.send("load event edit form");
-});
-
-// post route for edit
-app.post("/shakalakaboomboom/edit/:id", async (req, res) => {
-  try {
-    const id = req.params.id;
-    const secretPassed = req.body.secret;
-    if (secretPassed !== secret) {
-      return res.status(403).json({ msg: "Unauthorized" });
-    }
-    const { name, startTime, endTime, locName, lat, long } = req.body;
-
-    const updated = await Event.findByIdAndUpdate(
-      id,
-      {
-        name,
-        startTime,
-        endTime,
-        locations: [{ name: locName, latitude: lat, longitude: long }],
-      },
-      { new: true }
-    );
-
-    if (!updated) {
-      return res.status(404).json("event not found");
-    }
-    alert("Event updated");
-    res.redirect("/shakalakaboomboom");
-  } catch (e) {
-    console.error(e);
-    res.status(500).json({ msg: "Internal server error" });
-  }
-});
-
-// delete route
-app.post("/shakalakaboomboom/delete/:id", async (req, res) => {
-  if (req.body.secret !== secret) {
-    return res.status(403).json({ msg: "Unauthorized" });
-  }
-
-  const deleted = await Event.findByIdAndDelete(req.params.id);
-
-  if (!deleted) {
-    return res.status(404).json({ msg: "Event not found" });
-  }
-  alert("Event deleted");
-  res.redirect("admin");
-});
-
 
 // event search - case in-sensitive and with regular expressions
 app.get("/search", async (req, res) => {
   const query = req.query.q;
-  const events = await Event.find({
-    name: { $regex: query, $options: "i" },
-  });
+  const events = await loadJSONData('events.json');
+    
+  const regex = new RegExp(query, "i");
+  const filteredEvents = events.filter(event =>
+    regex.test(event.name) ||
+    (event.locations && event.locations.some(loc =>
+      regex.test(loc.name)
+    ))
+  );
+  // const events = await Event.find({
+  //   name: { $regex: query, $options: "i" },
+  // });
 
-  if (Object.keys(events).length > 0) {
-    return res.render("events", { events, query });
+  if (Object.keys(filteredEvents).length > 0) {
+    return res.render("events", { filteredEvents, query });
   }
   const f = "true"
-  return res.render("events", {events, f});
+  return res.render("events", {filteredEvents, f});
 });
 
 
